f3b2c7df4336ad0b722789895be346efb5ad661a
SamuelCano03 <U202116508@upc.edu.pe>
2024-06-15 18:35:18 -0500

    Adding visual effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import './App.css';
 import './myapp.css'
-
-// I want to use state in this component
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const loading = 'https://images.freeimages.com/image/previews/cd6/colorful-mandala-pattern-5691524.jpg';
+
+// Creating initial state
 const initialState = {
   init: true,
-  id: 0,
+  showMsg: true,
 }
 
 function App() {
+  // Using useState
   const [state, setState] = useState(initialState);
+
+  // Function to handle onClick event
   const onClick = () => {
     setState({ ...state, init: false });
   }
+
+  // useEffect to show message for 3 seconds
+  useEffect(() => {
+    if(!state.init){
+      const timer = setTimeout(() => {
+        setState({ ...state, showMsg: false});
+      }, 3000); //just 3 seconds
+      return () => clearTimeout(timer); //clear the timer
+    }
+  }, [state.init]);
+
+  // Conditional rendering
   if (state.init) {
     return (
       <div className="App">
@@ -25,57 +40,69 @@ function App() {
       </div>
     )
   }
-
-  return (
-    <div className="board-class bg-fixed bg-cover parallax">
-      <header className="myApp-header flex py-4 space-x-4">
-        <div className='mr-8 flex space-x-4'>
-          <p className='bold'>Zona:</p>
-          <select name="región" id="" className='bg-gray-500 rounded'>
-            <option value="">Lima</option>
-            <option value="">Rural</option>
-          </select>
-        </div>
-        <div className='rm-8 flex space-x-4'>
-          <p className='bold'>Internet:</p>
-          <select name="Internet" id="" className='bg-gray-500 rounded'>
-            <option value="">Buena</option>
-            <option value="">Medio</option>
-            <option value="">Malo</option>
-          </select>
-        </div>
-        <div className='rm-8 flex space-x-4'>
-          <p className='bold'>Dispositivos:</p>
-          <select name="Dispositivos" id="" className='bg-gray-500 rounded'>
-            <option value="">Tablet</option>
-            <option value="">Libros</option>
-          </select>
-        </div>
-      </header>
-      <main className='flex m-10'>
-        <div className='relative flex-grow'>
-          <div className='card-content flex'>
-            <img src="classroom.jpeg" alt='card-image' className='rounded-lg m-10 shadow-lg' />
-            <div className='rounded-lg bg-yellow-200 m-10 p-5 border-2 border-gray-800'>
-              <h2 className='text-lg font-bold'>Card Title</h2>
-              <p className='text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-            </div>
+  else{
+    return (
+      <div className="board-class bg-fixed bg-cover h-full parallax hide-scrollbar">
+        {
+          state.showMsg ? (
+          <div className='fade-in-out flex h-screen items-center justify-center'>
+            <p className='tesxt-center text-2xl font-bold'>Hello World</p>
           </div>
-        </div>
-      </main>
-      <main className='flex m-10'>
-        <div className='relative flex-grow'>
-          <div className='card-content flex'>
-            <img src="classroom.jpeg" alt='card-image' className='rounded-lg m-10 shadow-lg' />
-            <div className='rounded-lg bg-pink-200 m-10 p-5 border-2 border-gray-800'>
-              <h2 className='text-lg font-bold'>Card Title</h2>
-              <p className='text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+          ) : 
+          (
+            <div className='fade-in' >
+              <header className="myApp-header flex py-4 space-x-4">
+                <div className='mr-8 flex space-x-4'>
+                  <p className='bold'>Zona:</p>
+                  <select name="región" id="" className='bg-gray-500 rounded'>
+                    <option value="">Lima</option>
+                    <option value="">Rural</option>
+                  </select>
+                </div>
+                <div className='rm-8 flex space-x-4'>
+                  <p className='bold'>Internet:</p>
+                  <select name="Internet" id="" className='bg-gray-500 rounded'>
+                    <option value="">Buena</option>
+                    <option value="">Medio</option>
+                    <option value="">Malo</option>
+                  </select>
+                </div>
+                <div className='rm-8 flex space-x-4'>
+                  <p className='bold'>Dispositivos:</p>
+                  <select name="Dispositivos" id="" className='bg-gray-500 rounded'>
+                    <option value="">Tablet</option>
+                    <option value="">Libros</option>
+                  </select>
+                </div>
+              </header>
+              <main className='flex m-10'>
+                <div className='relative flex-grow'>
+                  <div className='card-content flex'>
+                    <img src="classroom.jpeg" alt='card-image' className='rounded-lg m-10 shadow-lg' />
+                    <div className='rounded-lg bg-yellow-200 m-10 p-5 border-2 border-gray-800'>
+                      <h2 className='text-lg font-bold'>Card Title</h2>
+                      <p className='text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    </div>
+                  </div>
+                </div>
+              </main>
+              <main className='flex m-10'>
+                <div className='relative flex-grow'>
+                  <div className='card-content flex'>
+                    <div className='rounded-lg bg-pink-200 m-10 p-5 border-2 border-gray-800'>
+                      <h2 className='text-lg font-bold'>Card Title</h2>
+                      <p className='text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    </div>
+                    <img src="classroom.jpeg" alt='card-image' className='rounded-lg m-10 shadow-lg' />
+                  </div>
+                </div>
+              </main>
             </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+          )
+        }
+      </div>
+    );
+  } 
 }
 
 export default App;
