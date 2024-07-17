75d1461a64052add62959762a5e3c26158ce6684
SamuelCano03 <102400537+SamuelCano03@users.noreply.github.com>
2024-07-17 11:30:05 -0500

    model agent

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const InitialScreen = ({ onClick }) => {
+  useEffect(() => {
+    // Cargar el CSS
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css';
+    document.head.appendChild(link);
+
+    // Cargar el script
+    const script = document.createElement('script');
+    script.src = 'https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Limpiar los elementos al desmontar el componente
+    return () => {
+      document.head.removeChild(link);
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header board-class bg-custom bg-cover bg-no-repeat">
         <img src="myfavi.png" className="App-logo" alt="logo" />
         <button onClick={onClick} className='bg-gray-500 hover:bg-blue-500 font-bold py-2 px-1 rounded'>Click me</button>
       </header>
+      {/* Insertar el código embebido */}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+          <df-messenger
+            location="us-central1"
+            project-id="gen-lang-client-0696680921"
+            agent-id="fd6c00c6-89bc-4cf1-863b-3133d5179aea"
+            language-code="en"
+            max-query-length="-1">
+            <df-messenger-chat-bubble chat-title="recreo agente"></df-messenger-chat-bubble>
+          </df-messenger>
+          <style>
+            df-messenger {
+              z-index: 999;
+              position: fixed;
+              --df-messenger-font-color: #000;
+              --df-messenger-font-family: Google Sans;
+              --df-messenger-chat-background: #f3f6fc;
+              --df-messenger-message-user-background: #d3e3fd;
+              --df-messenger-message-bot-background: #fff;
+              bottom: 16px;
+              right: 16px;
+            }
+          </style>
+          `,
+        }}
+      />
     </div>
   );
 };
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,11 +5,11 @@ import Header from './../components/Header';
 const MainContent = () => {
   return (
     <div className='container hide-scrollbar'>
-      <div className='flexi slide'>
+      <div className='slide slide1'>
         <img src="Lily.svg" alt="" />
       </div>
-      <div className='flexi slide slide2'>
-        <div className='w-2/5 mr-32 mt-10 shadow-lg '>
+      <div className='slide slide2'>
+        <div className='w-2/5 shadow-lg '>
           <h2 className='text-red-600 my-10 text-2xl text-center'>¿CUANTO GASTA EL ESTADO EN EDUCAR A UN NIÑO?</h2>
           <div className='bg-yellow-400 p-5 border-solid border-2 rounded-lg border-black'>
             <p>Desde una visión cuantitativa del gasto público, se busca analizar cuánto dinero presupuestal anual es destinado a educación y evaluar la cantidad individual de gasto estatal por niño. Se pretende ver si esta inversión se refleja en la realidad nacional, examinando el vínculo entre el gasto público y la actualidad educativa. Además, se buscará ligar la información con el capital humano y cuántos niños logran asistir a la universidad.</p>
@@ -19,7 +19,7 @@ const MainContent = () => {
       </div>
       <div className='slide slide3'>
         <Header/>
-        <div className='w-3/12 ml-96 mt-20'>
+        <div className='w-4/12 text-on-slide3'>
           <p className='text-white'>
             Los colegios tecnológicos son muy caros. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nihil fuga esse, ipsa, mollitia ipsum in et culpa vitae consectetur alias dolore accusantium. Consequatur et itaque asperiores consequuntur doloremque recusandae!
           </p>
